0b204e0ba1fe5cae7b3c9a3b676ea5d616c54ed4
Sarah Good <sarah.elizabeth.good@gmail.com>
2023-04-30 13:53:26 +0100

    Apply CA border, minor formatting changes

--- app.R
+++ app.R
@@ -19,19 +19,20 @@ server <- function(input, output, session) {
   ca_outline <- readOGR(dsn = 'shapefile/cb_2018_us_state_500k.shp', layer = "cb_2018_us_state_500k", GDAL1_integer64_policy = TRUE)
   ca_outline <- ca_outline[ca_outline$NAME == "California",]
   
+  # Apply projection
+  ca_outline <- sp::spTransform(ca_outline,projection)
+  
   # Merge Population Data
   df_small <- df[df$Year == 2021,]
   #places_map@data <- df_small %>% right_join(places_map@data, by = c("CITY"="CITY"))
   
-  # Render places map
-  ca_map <- ggplot() + geom_polygon(data = df_small, aes(x = long, y = lat, group = group), colour = "black", fill = NA)
   
   output$map <- renderLeaflet({
     leaflet(places_map) %>%
       addTiles() %>%
-      #addPolygons(data=ca_outline, fillColor='red', opacity = 1.0, fillOpacity = 0.5) %>%
-      addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
-                  opacity = 1.0, fillOpacity = 0.5)
+      addPolygons(color = "#444444", fillColor = 'blue', weight = 1, smoothFactor = 0.5,
+                  opacity = 1.0, fillOpacity = 0.5) %>%
+      addPolygons(data=ca_outline, color = '#444444', weight = 3, opacity = 1.0, fillOpacity = 0)
   })
 }
 shinyApp(ui, server)
